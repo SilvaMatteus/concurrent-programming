2d8d101444be72eb697f1668426e6280a5a8003a
Leulz <leo.vital@ccc.ufcg.edu.br>
2018-05-17 21:41:25 -0300

    Adding build.sh and run.sh scripts, still need to implement stop functionality in run.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mkdir -p nanoHTTPd-modified/bin && javac -d nanoHTTPd-modified/bin nanoHTTPd-modified/src/NanoHTTPd.java 2>/dev/null
---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+case $1 in
+    start)
+        if [ $# -ne 2 ]
+          then
+             echo "Usage: ./run.sh start [port]"
+        fi
+        java -classpath nanoHTTPd-modified/bin NanoHTTPd $2 &
+        ;;
+    *) echo "Option $1 not recognized." ;;
+esac
