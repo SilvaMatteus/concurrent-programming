44e5b69ed78186013ab75f2e34232540b2e0279a
Leulz <leo.vital@ccc.ufcg.edu.br>
2018-06-16 09:15:01 -0300

    Adding stop option to run.sh

--- run.sh
+++ run.sh
@@ -8,5 +8,11 @@ case $1 in
         fi
         java -classpath nanoHTTPd-modified/bin nanohttp.NanoHTTPd $2 &
         ;;
+    stop)
+        pid=$(ps aux | grep "java -classpath nanoHTTPd-modified/bin nanohttp.NanoHTTPd" | grep -v grep | awk '{print $2}')
+        if [ -n "$pid" ]; then
+          kill -9 $pid
+        fi
+        ;;
     *) echo "Option $1 not recognized." ;;
 esac
